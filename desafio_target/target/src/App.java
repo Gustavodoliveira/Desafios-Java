469d82bbc9e945565b85f58825c79ab9167813ba
Gustavo <gustavod.oliveira@outlook.com>
2024-09-03 06:21:46 -0300

    feat: 1° desafio resolvido

---
+++ desafio_target/target/src/App.java
@@ -0,0 +1,6 @@
+
+public class App {
+  public static void main(String[] args) throws Exception {
+    System.err.println(desafio.sum(13)); // 91 result
+  }
+}
---
+++ desafio_target/target/src/desafio.java
@@ -0,0 +1,18 @@
+public class desafio {
+
+  static int sum(int indice) {
+    int k = 0;
+    int sum = 0;
+    for (int i = 0; i < indice; i++) {
+      k = k + 1;
+      sum = sum + k;
+    }
+
+    return sum;
+  }
+
+  void fibonacci() {
+    int firtNumber = 1;
+    int twoNumber = 1;
+  }
+}
